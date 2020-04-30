45b3a7e56cb9b433729416aa34cddcab9e585aef
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-04-30 13:04:14 +0200

    JSX code

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-## Project has been created with Udemy courses <<Modern React with Redux>> - Stephan Grider.
+## Project has been created with Udemy courses <Modern React with Redux> - Stephan Grider.
 
-#8 >
-npx create-react-app my-react-app
-cd my-react-app
-code .
-npm start
+#8
+
+> npx create-react-app my-react-app
+> cd my-react-app
+> code .
+> npm start
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+  return "Click on Me!";
+}
+
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = "Click Me!";
+  const style = { backgroundColor: "blue", color: "white" };
+
+  const onButtonText = { text: "Click on Me!" };
+
+  const labelText = "Enter Name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}{" "}
+      </label>
+      <input type="text" id="name" />
+
+      <button style={style}>{buttonText}</button>
+
+      <button style={{ backgroundColor: "red", color: "white" }}>
+        {onButtonText.text}
+      </button>
+
+      <button style={{ backgroundColor: "green", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
