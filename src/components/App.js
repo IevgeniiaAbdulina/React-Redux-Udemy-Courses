abeffce0459f70fdf47aeffe5c5685c70d94fc6f
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-05-08 18:48:19 +0200

    Component Design
    // event handlers
    // handling form submittal

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "20px" }}>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
