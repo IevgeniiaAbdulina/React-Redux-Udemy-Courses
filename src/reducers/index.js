aaeb2001e5df435ca008320dac0690447d6058b5
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-05-12 18:10:44 +0200

    Added reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,31 @@
+const songReducer = () => {
+  return [
+    {
+      title: "Rock and Roll (Led Zeppelin)",
+      duration: "4:05",
+    },
+    {
+      title: "Back in the U.S.S.R. (Beatles)",
+      duration: "3:50",
+    },
+    {
+      title: "Hold On Tight (Electric Light Orchestra)",
+      duration: "3:06",
+    },
+    {
+      title: "Trouble (Keith Richards)",
+      duration: "4:18",
+    },
+    {
+      title: "Candy Store Rock (Led Zeppelin)",
+      duration: "4:10",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
