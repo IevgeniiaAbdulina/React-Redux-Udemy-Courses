c578ec296df84dcb16dd0d559387d461715cf9e1
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2020-05-22 16:51:29 +0200

    Implementing Programmatic navigation

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -32,6 +33,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   });
   // Do some programmatic navigation to
   // get the user back to the rot route
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
